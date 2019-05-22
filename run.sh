ee568919e4db7778366dd3e7d0967705855abf58
tripleCC <triplec.linux@gmail.com>
2019-05-22 12:28:07 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "配置插件"
+pod bin init --bin-url=https://github.com/for-example-test/cocoapods-bin-example/raw/master/bin.yml
+
+echo "启动静态服务器"
+cd binary-server
+npm install
+npm start &
+
+echo "install 工程"
+cd ..
+cd Demo
+pod install --verbose
