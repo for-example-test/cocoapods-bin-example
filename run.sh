8268332833f03e77aead518e840e19f0cb79a358
tripleCC <triplec.linux@gmail.com>
2019-05-22 12:38:12 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "配置插件"
-pod bin init --bin-url=https://github.com/for-example-test/cocoapods-bin-example/raw/master/bin.yml
+pod bin init --bin-url=https://raw.githubusercontent.com/for-example-test/cocoapods-bin-example/master/bin.yml
 
 echo "启动静态服务器"
 cd binary-server
@@ -12,3 +12,4 @@ echo "install 工程"
 cd ..
 cd Demo
 pod install --verbose
+pod bin
